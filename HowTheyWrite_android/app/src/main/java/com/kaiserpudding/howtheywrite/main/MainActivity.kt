e407ab701b836e6aa8523a11fa910ce06d58d5f2
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-03-29 21:20:38 +0100

    convert mainActivity to kotlin

---
+++ HowTheyWrite_android/app/src/main/java/com/kaiserpudding/howtheywrite/main/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.kaiserpudding.howtheywrite.main
+
+import android.content.Context
+import android.content.Intent
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.kaiserpudding.howtheywrite.characterList.CharacterListActivity
+import com.kaiserpudding.howtheywrite.R
+import com.kaiserpudding.howtheywrite.database.ChineseDbHelper
+import com.kaiserpudding.howtheywrite.lessonList.LessonListActivity
+import com.kaiserpudding.howtheywrite.lessonList.LessonListViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private val lessonViewModel: LessonListViewModel? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val databaseInitializer = DatabaseInitializer()
+        databaseInitializer.execute(this)
+    }
+
+    fun toLessons(view: View) {
+        val intent = Intent(this, LessonListActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun toCharacters(view: View) {
+        val intent = Intent(this, CharacterListActivity::class.java)
+        startActivity(intent)
+    }
+
+    private inner class DatabaseInitializer : AsyncTask<Context, Void, Void>() {
+
+        override fun doInBackground(vararg contexts: Context): Void? {
+            val a = ChineseDbHelper(contexts[0])
+            a.readableDatabase
+            return null
+        }
+    }
+
+}
