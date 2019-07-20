448c558618891415f84816b2c0e6d4694ca6d226
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-07-20 16:39:31 +0200

    Fix "file not found bug" when copying db from assets
    Create /database directory if it is missing

--- HowTheyWrite_android/app/src/main/java/com/kaiserpudding/howtheywrite/database/ChineseDbHelper.kt
+++ HowTheyWrite_android/app/src/main/java/com/kaiserpudding/howtheywrite/database/ChineseDbHelper.kt
@@ -21,7 +21,14 @@ class ChineseDbHelper(val context: Context) : SQLiteOpenHelper(context, DATABASE
 
     private fun installDatabaseFromAssets() {
         val inputStream = context.assets.open("databases/$DATABASE_NAME")
-        val outputFile = File(context.getDatabasePath(DATABASE_NAME).path)
+        val fullPath = context.getDatabasePath(DATABASE_NAME).path
+
+        // create database folder if it does no exist yet
+        val folderPath = fullPath.substring(0, fullPath.length - DATABASE_NAME.length - 1)
+        val file = File(folderPath)
+        if (!file.isDirectory) file.mkdir()
+
+        val outputFile = File(fullPath)
         val outputStream = FileOutputStream(outputFile)
 
         inputStream.copyTo(outputStream)
@@ -33,11 +40,11 @@ class ChineseDbHelper(val context: Context) : SQLiteOpenHelper(context, DATABASE
     }
 
     private fun installedDatabaseIsOutdated(): Boolean {
-        return preferences.getInt(DATABASE_NAME,0) < DATABASE_VERSION
+        return preferences.getInt(DATABASE_NAME, 0) < DATABASE_VERSION
     }
 
     private fun writeDatabaseVersionInPreferences() {
-        preferences.edit().apply{
+        preferences.edit().apply {
             putInt(DATABASE_NAME, DATABASE_VERSION)
             apply()
         }
